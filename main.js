6bea692eedb40b959b68a0f6ae5914f78448e1f8
Prince-luck <herbertfortune12@gmail.com>
2024-07-18 15:36:01 +0100

    wolf

--- main.js
+++ main.js
@@ -208,7 +208,7 @@ try{
             console.log(color(`${themeemoji} WA NUMBER: ${owner}`,'magenta'))
             console.log(color(`${themeemoji} CREDIT: ${wm}\n`,'magenta'))
             // send a message when the bot goes online
-            XeonBotInc.ev.on('message.new', async (message) => {'WHATSAPP BOT online'})
+            XeonBotInc.sendMessage(global.owner, "Wolf Tech")
 		}
 	
 } catch (err) {
